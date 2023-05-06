981873e6a8b643c7b8c3b3cdf3790724921a619c
gsubhampatra <gpb021@nist.edu>
2023-05-06 21:58:25 +0530

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const connectToMongo = require('./db');
 const express = require('express')
 const auth = require('./routes/auth');
 const notes = require('./routes/notes');
+const cors = require('cors')
 connectToMongo()
 const app = express()
 const port = process.env.PORT || 3030
 require('dotenv').config()
 
 app.use(express.json())
+app.use(cors)
 
 app.use('/api/auth',auth)
 app.use('/api/notes',notes)
