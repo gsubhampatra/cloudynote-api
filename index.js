67602848da795de8548406262d0b1c270f94967d
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2023-05-07 21:29:13 +0530

    fix cors for all requests

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ require('dotenv').config()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://cloudynote.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 app.use('/api/auth',auth)
 app.use('/api/notes',notes)
 
