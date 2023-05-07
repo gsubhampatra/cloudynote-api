d9bd853f1164f44f8d69ad9c36433298d0424293
G. Subham Kumar Patra <96821893+gsubhampatra@users.noreply.github.com>
2023-05-07 22:00:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
